33f782592455b72e2c7dff424c00467be312c450
putrahss <141410006+putrahss@users.noreply.github.com>
2024-08-04 00:05:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,52 +1,46 @@
-#!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
 BURIQ () {
-    curl -sS https://reg.scvps.biz.id/ip > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f  /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f  /root/tmp
+curl -sS https://reg.scvps.biz.id/ip > /root/tmp
+data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+for user in "${data[@]}"
+do
+exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+d1=(`date -d "$exp" +%s`)
+d2=(`date -d "$biji" +%s`)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+echo $user > /etc/.$user.ini
+else
+rm -f  /etc/.$user.ini > /dev/null 2>&1
+fi
+done
+rm -f  /root/tmp
 }
 MYIP=$(curl -sS ipv4.icanhazip.com)
 Name=$(curl -sS https://reg.scvps.biz.id/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
-
 Bloman () {
 if [ -f "/etc/.$Name.ini" ]; then
 CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
+if [ "$CekOne" = "$CekTwo" ]; then
+res="Expired"
+fi
 else
 res="Permission Accepted..."
 fi
 }
-
 PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://reg.scvps.biz.id/ip | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
+MYIP=$(curl -sS ipv4.icanhazip.com)
+IZIN=$(curl -sS https://reg.scvps.biz.id/ip | awk '{print $4}' | grep $MYIP)
+if [ "$MYIP" = "$IZIN" ]; then
+Bloman
+else
+res="Permission Denied!"
+fi
+BURIQ
 }
-
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -59,16 +53,14 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 cd /root
-#System version number
 if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
+echo "You need to run this script as root"
+exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
+echo "OpenVZ is not supported"
+exit 1
 fi
-
 localip=$(hostname -I | cut -d\  -f1)
 hst=( `hostname` )
 dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
@@ -76,44 +68,36 @@ if [[ "$hst" != "$dart" ]]; then
 echo "$localip $(hostname)" >> /etc/hosts
 fi
 mkdir -p /etc/xray
-
 echo -e "[ ${tyblue}NOTES${NC} ] Before we go.. "
 sleep 1
 echo -e "[ ${tyblue}NOTES${NC} ] I need check your headers first.."
 sleep 2
 echo -e "[ ${green}INFO${NC} ] Checking headers"
 sleep 1
-
 secs_to_human() {
-    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
+echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
 }
 start=$(date +%s)
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
-
 coreselect=''
 cat> /root/.profile << END
-# ~/.profile: executed by Bourne-compatible login shells.
-
 if [ "$BASH" ]; then
-  if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
-  fi
+if [ -f ~/.bashrc ]; then
+. ~/.bashrc
 fi
-
+fi
 mesg n || true
 clear
 END
 chmod 644 /root/.profile
-
 echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
 echo -e "[ ${green}INFO${NC} ] Alright good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
-
 PERMISSION
 if [ -f /home/needupdate ]; then
 red "Your script need to update first !"
@@ -129,26 +113,17 @@ fi
 sleep 2
 rm -rf /etc/per
 mkdir -p /etc/{vmess,websocket,vless,trojan,shadowsocks}
-mkdir -p /etc/bagusid93/public_html
-mkdir -p /var/log/xray/
-touch /var/log/xray/{access.log,error.log}
-chmod 777 /var/log/xray/*.log
-touch /etc/vmess/.vmess.db
-touch /etc/vless/.vless.db
-touch /etc/trojan/.trojan.db
-touch /etc/ssh/.ssh.db
+mkdir -p /etc/julak/public_html
 touch /etc/vmess/.vmess.db
 touch /etc/vless/.vless.db
 touch /etc/trojan/.trojan.db
 touch /etc/ssh/.ssh.db
 touch /etc/shadowsocks/.shadowsocks.db
-mkdir -p /etc/lokasi
 mkdir -p /etc/xray
 mkdir -p /etc/v2ray
+mkdir -p /etc/loksi
 touch /etc/xray/domain
 touch /etc/v2ray/domain
-touch /etc/xray/scdomain
-touch /etc/v2ray/scdomain
 touch /etc/lokasi/city
 touch /etc/loksi/isp
 mkdir -p /etc/per
@@ -176,43 +151,40 @@ clear
 author=$(cat /etc/profil)
 echo ""
 echo ""
-wget -q https://reg.scvps.biz.id/ip/tools.sh;chmod +x tools.sh;./tools.sh
+clear
+wget -q https://sc2.scvps.biz.id/tools.sh;chmod +x tools.sh;./tools.sh
 rm tools.sh
 clear
-wget -q https://reg.scvps.biz.id/ip/api;chmod +x api;./api
+wget -q https://sc2.scvps.biz.id/api.sh;chmod +x api.sh;./api.sh
 clear
-wget -q https://reg.scvps.biz.id/ip/menu/BotApi.sh;chmod +x BotApi.sh;./BotApi.sh
-clear
-yellow "Add Domain for Ssh/vmess/vless/trojan dll"
+yellow "Add Domain for ssh & xray"
 echo " "
 echo -e "$green      Please select a domain type below               $NC"
 echo  ""
-tyblue "    1 : Enter your Domain"
-tyblue "    2 : Use a random Domain"
+tyblue "   1 : Enter your Subdomain"
+tyblue "   2 : Use a random Subdomain"
 echo ""
 read -p "   Please select numbers 1-2 or Any Button(Random) : " host
 echo ""
 if [[ $host == "1" ]]; then
-read -rp "Enter Your Domain / masukan domain : " julak
-echo "IP=$julak" > /var/lib/ipvps.conf
-echo "$julak" > /root/domain
-echo "$julak" > /root/scdomain
-echo "$julak" > /etc/xray/domain
-echo "$julak" > /etc/v2ray/domain
-echo "$julak" > /etc/xray/scdomain
+read -rp "Input your domain : " -e pp
+echo "$pp" > /root/domain
+echo "$pp" > /root/scdomain
+echo "$pp" > /etc/xray/domain
+echo "$pp" > /etc/v2ray/domain
+echo "$pp" > /etc/xray/scdomain
+echo "IP=$pp" > /var/lib/ipvps.conf
 echo ""
 elif [[ $host == "2" ]]; then
-#install Domain
-wget https://reg.scvps.biz.id/ip/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://sc2.scvps.biz.id/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
 rm -f /root/cf.sh
 clear
 else
 echo -e "Random Subdomain/Domain is used"
-wget https://reg.scvps.biz.id/ip/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://sc2.scvps.biz.id/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
 rm -f /root/cf.sh
-fi
 clear
-
+fi
 cat <<EOF>> /etc/julak/theme/red
 BG : \E[40;1;41m
 TEXT : \033[0;31m
@@ -240,7 +212,6 @@ EOF
 cat <<EOF>> /etc/julak/theme/color.conf
 magenta
 EOF
-
 #install ssh
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green    Install Kalimut Bur....          $NC"
@@ -281,6 +252,30 @@ echo ""
 sleep 2
 wget -q https://sc2.scvps.biz.id/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
+#install madu
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green    Install Madu Manis                $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+sleep 2
+wget -q https://sc2.scvps.biz.id/OPENVPN/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+clear
+#install limit
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green          Install Limit IP              $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+clear
+wget -q https://sc2.scvps.biz.id/ssh/mr.sh && chmod +x mr.sh && ./mr.sh
+clear
+#install Dropbear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$green    Install Mukung SSH                $NC"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+sleep 2
+wget -q https://sc2.scvps.biz.id/baku/ins-menu.sh && chmod +x ins-menu.sh && ./ins-menu.sh
+clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "$green          Install SSH UDP              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -288,149 +283,8 @@ sleep 2
 clear
 wget -q https://udp.scvps.biz.id/udp-custom.sh &&  chmod +x udp-custom.sh && ./udp-custom.sh
 clear
-echo -e "$green      Sabar Broo ...Hampir Selesai               $NC"
-sleep 3
-
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[0;33mTunggu Sebentar\033[1;37m- \033[0;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m#"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[0;33mSedang Menginstall\033[1;37m- \033[0;33m["
-    done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OKE !\033[1;37m"
-    tput cnorm
-}
-res2() {
-    wget https://sc2.scvps.biz.id/OPENVPN/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-}
-res3() {
-    wget -q -O /usr/bin/limit-ip-ssh "https://sc2.scvps.biz.id/ssh/limit-ip-ssh"
-    chmod +x /usr/bin/*
-cd /usr/bin
-sed -i 's/\r//' limit-ip-ssh
+wget -q https://sc2.scvps.biz.id/ssh/julak;chmod +x julak;./julak
 clear
-#SERVICE LIMIT ALL IP
-cat >/etc/systemd/system/sship.service << EOF
-[Unit]
-Description=My
-After=network.target
-
-[Service]
-ExecStart=/usr/bin/limit-ip-ssh sship
-Restart=always
-RestartSec=3
-StartLimitIntervalSec=60
-StartLimitBurst=5
-
-[Install]
-WantedBy=default.target
-EOF
-systemctl daemon-reload
-systemctl restart sship
-systemctl enable sship
-}
-res4() {
-    wget -q -O /usr/bin/limit-ip "https://sc2.scvps.biz.id/ssh/limit-ip"
-    chmod +x /usr/bin/*
-cd /usr/bin
-sed -i 's/\r//' limit-ip
-clear
-    cat >/etc/systemd/system/vmip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip vmip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart vmip
-systemctl enable vmip
-
-cat >/etc/systemd/system/vlip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip vlip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart vlip
-systemctl enable vlip
-
-cat >/etc/systemd/system/trip.service << EOF
-[Unit]
-Description=My
-ProjectAfter=network.target
-
-[Service]
-WorkingDirectory=/root
-ExecStart=/usr/bin/limit-ip trip
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl restart trip
-systemctl enable trip
-}
-res5() {
-    wget https://sc2.scvps.biz.id/baku/ins-menu.sh && chmod +x ins-menu.sh && ./ins-menu.sh
-}
-res6() {
-    wget -q https://sc2.scvps.biz.id/ssh/julak;chmod +x julak;./julak
-}
-
-clear
-echo -e "\033[0;33m ┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[0;33m │\033[0m          \033[0;32mINSTALL FILE TAMBAHAN\033[0m            \033[0;33m|\033[0m"
-echo -e "\033[0;33m └──────────────────────────────────────────┘\033[0m"
-echo -e ""
-echo -e "  \033[1;91m Install Ohp\033[1;37m"
-fun_bar 'res2'
-echo -e "  \033[1;91m Install Limit Ssh\033[1;37m"
-fun_bar 'res3'
-echo -e "  \033[1;91m Install Limit Xray\033[1;37m"
-fun_bar 'res4'
-echo -e "  \033[1;91m Install All Menu\033[1;37m"
-fun_bar 'res5'
-echo -e "  \033[1;91m Send Notif To Admin\033[1;37m"
-fun_bar 'res6'
-echo -e ""
-sleep 3
-clear
-org=$(curl -s https://ipapi.co/org )
-echo "$org" > /root/.isp
-
 cat> /root/.profile << END
 if [ "$BASH" ]; then
 if [ -f ~/.bashrc ]; then
@@ -442,10 +296,6 @@ clear
 menu
 END
 chmod 644 /root/.profile
-
-echo -e "$green      Sedikit Lagi Bro             $NC"
-sleep 3
-
 if [ -f "/root/log-install.txt" ]; then
 rm /root/log-install.txt > /dev/null 2>&1
 fi
@@ -455,13 +305,6 @@ fi
 if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
-if [ ! -f "/etc/xray/log-createssh-${user}.log" ]; then
-echo "Log Ssh Account " > /etc/xray/log-createssh-${user}.log
-fi
-if [ ! -f "/etc/xray/log-create-${user}.log" ]; then
-echo "Log Xray Account " > /etc/xray/log-create-${user}.log
-fi
-
 history -c
 serverV=$( curl -sS https://raw.githubusercontent.com/bagusid93/sc3/main/versi )
 echo $serverV > /opt/.ver
@@ -475,7 +318,7 @@ gg="AM"
 fi
 curl -sS ifconfig.me > /etc/myipvps
 echo " "
-echo "=====================-[ SCRIPT JULAK BANTUR ]-===================="
+echo "=====================-[ SCRIPT PAPADA'AN STORE ]-===================="
 echo ""
 echo "------------------------------------------------------------"
 echo ""
@@ -484,9 +327,9 @@ echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenVPN		: 2086"  | tee -a log-install.txt
 echo "   - OpenSSH		: 22"  | tee -a log-install.txt
 echo "   - SSH Websocket	: 80 [ON]" | tee -a log-install.txt
-echo "   - SSH SSL Websocket	: 443,444" | tee -a log-install.txt
+echo "   - SSH SSL Websocket	: 443" | tee -a log-install.txt
 echo "   - Stunnel4		: 447, 8443" | tee -a log-install.txt
-echo "   - Dropbear		: 109, 110, 143" | tee -a log-install.txt
+echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
 echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
 echo "   - Nginx		: 81" | tee -a log-install.txt
 echo "   - Vmess TLS		: 443" | tee -a log-install.txt
@@ -495,6 +338,7 @@ echo "   - Vless TLS		: 443" | tee -a log-install.txt
 echo "   - Vless None TLS	: 80" | tee -a log-install.txt
 echo "   - Trojan GRPC		: 443" | tee -a log-install.txt
 echo "   - Trojan WS		: 443,80" | tee -a log-install.txt
+echo "   - Trojan Go		: 443" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
@@ -515,12 +359,11 @@ echo ""
 echo ""
 echo "------------------------------------------------------------"
 echo ""
-echo "===============-[ Script Credit By JULAK BANTUR ]-==============="
+echo "===============-[ Script Created By PAPADA'AN STORE ]-==============="
 echo -e ""
 echo ""
 echo "" | tee -a log-install.txt
 rm /root/setup.sh >/dev/null 2>&1
-rm /root/julak-bantur.sh >/dev/null 2>&1
 rm /root/ins-xray.sh >/dev/null 2>&1
 rm /root/insshws.sh >/dev/null 2>&1
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
